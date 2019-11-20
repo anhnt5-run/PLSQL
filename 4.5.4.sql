9f41746cc9cd0a16bb87ec3b86bcb89b04a36e3c
Nguyen Tuan Anh <anhnt5@runsystem.net>
2019-11-20 17:32:43 +0700

    update format

--- 4.5.4.sql
+++ 4.5.4.sql
@@ -6,12 +6,21 @@
 --------------------------------------------------------
 
   CREATE OR REPLACE EDITIONABLE TRIGGER "RUNSYSTEM"."TRIG_AVG_SCORE" 
-  FOR DELETE OR INSERT OR UPDATE ON score COMPOUND TRIGGER avg_score 
-score.score%TYPE; avg_score_by_credit_num score.score%TYPE; AFTER EACH ROW IS BEGIN 
-SELECT avg(score) INTO avg_score FROM score WHERE student_id = :NEW.student_id 
-GROUP BY subjects_id; SELECT (SUM(sc.score * sub.credit_num) / 
-SUM(sub.credit_num)) INTO avg_score_by_credit_num FROM score sc inner join subjects sub ON 
-sc.subjects_id = sub.id WHERE sc.student_id = :NEW.student_id; END AFTER EACH ROW; END 
+FOR DELETE OR INSERT OR UPDATE ON score 
+COMPOUND TRIGGER 
+avg_score score.score%TYPE; 
+avg_score_by_credit_num score.score%TYPE; 
+
+    AFTER EACH ROW IS 
+    BEGIN 
+        SELECT avg(score) INTO avg_score FROM score WHERE student_id = :NEW.student_id 
+        GROUP BY subjects_id; 
+        
+        SELECT (SUM(sc.score * sub.credit_num) / 
+        SUM(sub.credit_num)) INTO avg_score_by_credit_num FROM score sc inner join subjects sub ON 
+        sc.subjects_id = sub.id WHERE sc.student_id = :NEW.student_id; 
+    END AFTER EACH ROW; 
+END 
 trig_avg_score;
 /
 ALTER TRIGGER "RUNSYSTEM"."TRIG_AVG_SCORE" ENABLE;
